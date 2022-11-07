258b41c7a6b1573a0136f955f69dfef26ba00736
delower68 <hdelower68@gmail.com>
2022-11-08 00:48:59 +0800

    inistial setup and servies.json file created

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000 ;
+
+
+
+
+
+// middle ware 
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req ,res)=>{
+    res.send('The server is running');
+});
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+});
+
