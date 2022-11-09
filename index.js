fe6b08d20f10580bf4902f5df4210fcc934b7863
delower68 <hdelower68@gmail.com>
2022-11-09 13:41:25 +0800

    .env file remove and json file error set

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -15,11 +16,22 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wdnu49k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+
+const services = require('./data/services.json')
 
 app.get('/', (req ,res)=>{
     res.send('The server is running');
 });
 
+
+// get the all services here 
+app.get('/services', (req, res)=>{
+    res.send(services);
+})
 
 
 
