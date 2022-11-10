618bd150c1264b0182735f3d7ab8aaf64525befa
delower68 <hdelower68@gmail.com>
2022-11-10 23:36:44 +0800

    all set up done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
+const jwt = require('jsonwebtoken')
 
 const port = process.env.PORT || 5000 ;
 
@@ -21,22 +21,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-function verifyJWT(req, res, next){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send({message: "Unauthorized access"})
-    }
-    const token = authHeader.split(' ')[1];
-    // console.log(token);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRECT , function(err, decoded){
-        if(err){
-            res.status(403).send({message: "Forbidden access" })
-        }
-        req.decoded= decoded ;
-        next();
-    })
-
-}
 
 async function run(){
     try{
@@ -66,16 +50,8 @@ async function run(){
             const service = await serviceCollection.findOne(query)
             res.send(service);
         });
-        app.get('/reviews/:id' , async(req, res)=>{
-            let query = {};
-            if(req.query.
-                service){
-                query= req.query.service;
-            }
-            const cursor = reviewsCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        });
+
+        
 
         // user review data 
         app.post('/reviews', async(req, res)=>{
@@ -84,15 +60,7 @@ async function run(){
             res.send(result) 
         })
 
-        // reviews api receide here 
-        app.get('/reviews',verifyJWT, async(req, res)=>{
-
-            const decoded = req.decoded;
-            // console.log(decoded);
-            if(decoded.email !== req.query.email ){
-                res.status(403).send({message: "Forbidden access"})
-            }
-
+        app.get('/reviews', async(req, res)=>{
             let query = {};
             if(req.query.email){
                 query={
@@ -121,53 +89,30 @@ async function run(){
             res.send(result)
         });
         
-        // app.put('/reviews/:id' , async(req, res)=>{
-        //     const id =req.params.id;
-        //     const filter = {_id: ObjectId(id)};
-        //     const updatedReview = req.body;
-        //     console.log(updatedReview);
-        //     const options = {upsert: true};
-        //     const updatedDoc = {
-        //         $set: {
-        //             message: updatedReview.message
-        //         }
-        //     }
-        //     const result = await reviewsCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result);
-        // })
-
-        app.put('/reviews/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const user = req.body;
+        app.put('/reviews/:id' , async(req, res)=>{
+            const id =req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const updatedReview = req.body;
             const option = {upsert: true};
-            const updatedUser = {
+            const updatedDoc = {
                 $set: {
-                    message: user.message,
+                    message: updatedReview.message
                 }
             }
-            const result = await reviewsCollection.updateOne(filter, updatedUser, option);
+            const result = await reviewsCollection.updateOne(filter, updatedDoc, option);
             res.send(result);
         })
-
 
 
 
         // add a service here 
         app.post('/addService', async(req, res)=>{
             const addService = req.body ;
-            console.log(addService);
             const result = await serviceCollection.insertOne(addService);
             res.send(result);
         })
 
-        // for jwt 
-        app.post('/jwt', async(req, res)=>{
-            const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRECT, {expiresIn: '2h'})
-            res.send({token});
-            // console.log(token);
-        })
+       
 
     }
     finally{
